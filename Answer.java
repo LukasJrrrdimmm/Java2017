dc0576a22d7359c012f8f8b1fec8866a5e3a69cb
Lukas Maximo Grilo Abreu Jardim <lukasmaximo@gmail.com>
2017-05-05 19:59:08 -0300

    Add files via upload

---
+++ Answer.java
@@ -0,0 +1,19 @@
+
+public class Answer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 30, b = 18, c = 2;
+		int horse = 30/3;
+		int clank = (b - 10)/2;
+		int boot = -(2 - clank)/2;
+		System.out.println(boot + clank + horse);
+		System.out.println("boot = " + boot);
+		System.out.println("clank = " + clank);
+		System.out.println("horse = " + horse);
+	}
+
+}
